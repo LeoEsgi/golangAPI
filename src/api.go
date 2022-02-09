cd05f3a9fe7ed45732c926a218c8d43ce00d642c
LeoEsgi <dleo@myges.fr>
2022-02-09 07:55:57 -0800

    tpApi

--- src/api.go
+++ src/api.go
@@ -8,16 +8,18 @@ import (
 	"strconv"
 )
 
+// Exemple json : {"id" :,"name" : "Osiris","inPark" : "Asterix","place" : "France","manufacturer" : "Vortex"}
+
 type Attraction struct {
-	Id           int
-	Name         string
-	InPark       string
-	Manufacturer string
+	Id           int    `json:"Id"`
+	Name         string `json:"Name"`
+	InPark       string `json:"InPark"`
+	Place        string `json:"Place"`
+	Manufacturer string `json:"Manufacturer"`
 }
 
 func delete(id string, attr []Attraction) {
 
-	fmt.Println(id)
 	var int_id, _ = strconv.Atoi(id)
 	fmt.Println("Tableau de base :", attr)
 
@@ -34,22 +36,32 @@ func delete(id string, attr []Attraction) {
 
 }
 
-func patch(id string, attr []Attraction, name string, inPark string, manufacturer string) {
-	fmt.Println(attr)
+func patch(id string, attr []Attraction, name string, inPark string, place string, manufacturer string) []Attraction {
 	var int_id, _ = strconv.Atoi(id)
 	for i := 0; i < len(attr); i++ {
 		fmt.Println(int_id)
 		if attr[i].Id == int_id {
 
-			attr[i].Name = name
-			attr[i].InPark = inPark
-			attr[i].Manufacturer = manufacturer
+			if len(name) != 0 {
+				attr[i].Name = name
+			}
+			if len(inPark) != 0 {
+				attr[i].InPark = inPark
+			}
+			if len(manufacturer) != 0 {
+				attr[i].Manufacturer = manufacturer
+			}
+			if len(place) != 0 {
+				attr[i].Place = place
+			}
 			fmt.Println("Attraction updated : ", attr[i])
+			encode(attr[i])
 			break
 		}
 
 	}
-	fmt.Println(attr)
+
+	return attr
 }
 
 func get(attr []Attraction) {
@@ -70,70 +82,77 @@ func exist(attr []Attraction, id int) bool {
 	return false
 }
 
-func create(attr []Attraction, name string, inPark string, manufacturer string) {
+func create(attr []Attraction, name string, inPark string, place string, manufacturer string) []Attraction {
 	var id int
 	for {
-		id = rand.Intn(100)
+		id = rand.Intn(100000000000)
 		if exist(attr, id) {
 			continue
 
 		}
 		break
 	}
-	var attraction = Attraction{id, name, inPark, manufacturer}
+	var attraction = Attraction{id, name, inPark, place, manufacturer}
+
 	attr = append(attr, attraction)
-	fmt.Println(attr)
+	encode(attraction)
+	return attr
 
 }
 
-func encode() {
-	var id = rand.Intn(100)
-	s1 := &Attraction{id, "lÉO", "AD", "be"}
+func encode(s1 Attraction) {
+
 	bytes_s1, err := json.Marshal(s1)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(string(bytes_s1))
-	decode(string(bytes_s1))
 }
 
-func decode(s1 string) {
+func decode(s1 []byte) {
+
 	var stu = &Attraction{}
-	var err = json.Unmarshal([]byte(s1), stu)
+	var err = json.Unmarshal(s1, stu)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Unmarshal: Id : %d Name: %s, InPark: %s , Manufacturer : %s \n", stu.Id, stu.Name, stu.InPark, stu.Manufacturer)
+	fmt.Printf("Unmarshal: Name: %s, InPark: %s ,Place : %s  ,  Manufacturer : %s \n", stu.Name, stu.InPark, stu.Place, stu.Manufacturer)
 }
 
 func router() {
 
-	var size = 2
+	var size = 0
 	attractions := make([]Attraction, size)
-	attractions[0] = Attraction{1, "Léo", "Blob", "GB"}
-	attractions[1] = Attraction{2, "Nacer", "Blob", "GB"}
 
-	http.HandleFunc("/", home)
 	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
 		get(attractions)
 	})
 	http.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request) {
-		var id = r.FormValue("id")
+		var id = r.FormValue("Id")
 		delete(id, attractions)
 	})
 	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
-		var name = r.FormValue("Name")
-		var inPark = r.FormValue("InPark")
-		var manufacturer = r.FormValue("Manufacturer")
-		create(attractions, name, inPark, manufacturer)
+		fmt.Println("creation en cours ... : ")
+		name := r.FormValue("Name")
+		place := r.FormValue("Place")
+		manufacturer := r.FormValue("Manufacturer")
+		inPark := r.FormValue("InPark")
+
+		attractions = create(attractions, name, inPark, place, manufacturer)
+		fmt.Println(attractions)
+
 	})
 
 	http.HandleFunc("/patch", func(w http.ResponseWriter, r *http.Request) {
-		var name = r.FormValue("Name")
-		var inPark = r.FormValue("InPark")
-		var manufacturer = r.FormValue("Manufacturer")
-		var id = r.FormValue("Id")
-		patch(id, attractions, name, inPark, manufacturer)
+		fmt.Println("update en cours ... : ")
+		id := r.FormValue("Id")
+		name := r.FormValue("Name")
+		place := r.FormValue("Place")
+		manufacturer := r.FormValue("Manufacturer")
+		inPark := r.FormValue("InPark")
+
+		patch(id, attractions, name, inPark, place, manufacturer)
+		fmt.Println(attractions)
 
 		//	patch(id, attractions, name, inPark, manufacturer)
 
