25bd02c5a935f7250dd413869ce5d870bd72d898
LeoEsgi <dleo@myges.fr>
2022-02-09 10:09:38 -0800

    final

--- src/api.go
+++ src/api.go
@@ -3,12 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"strconv"
 )
-
-// Exemple json : {"id" :,"name" : "Osiris","inPark" : "Asterix","place" : "France","manufacturer" : "Vortex"}
 
 type Attraction struct {
 	Id           int    `json:"Id"`
@@ -16,6 +15,26 @@ type Attraction struct {
 	InPark       string `json:"InPark"`
 	Place        string `json:"Place"`
 	Manufacturer string `json:"Manufacturer"`
+}
+
+func saveInfile(attr []Attraction) {
+
+	for i := 0; i < len(attr); i++ {
+		fmt.Print("hello")
+		data := Attraction{
+
+			Id:           attr[i].Id,
+			Name:         attr[i].Name,
+			InPark:       attr[i].InPark,
+			Place:        attr[i].Place,
+			Manufacturer: attr[i].Manufacturer,
+		}
+
+		file, _ := json.MarshalIndent(data, "", " ")
+
+		_ = ioutil.WriteFile("test.json", file, 0644)
+
+	}
 }
 
 func delete(id string, attr []Attraction) {
@@ -33,7 +52,6 @@ func delete(id string, attr []Attraction) {
 	}
 
 	fmt.Println("Tableau delete : ", attr)
-
 }
 
 func patch(id string, attr []Attraction, name string, inPark string, place string, manufacturer string) []Attraction {
@@ -55,18 +73,13 @@ func patch(id string, attr []Attraction, name string, inPark string, place strin
 				attr[i].Place = place
 			}
 			fmt.Println("Attraction updated : ", attr[i])
-			encode(attr[i])
+
 			break
 		}
 
 	}
 
 	return attr
-}
-
-func get(attr []Attraction) {
-	fmt.Print(attr)
-
 }
 
 func exist(attr []Attraction, id int) bool {
@@ -93,39 +106,46 @@ func create(attr []Attraction, name string, inPark string, place string, manufac
 		break
 	}
 	var attraction = Attraction{id, name, inPark, place, manufacturer}
-
+	json := encode(attraction)
+	attraction = decode(json)
 	attr = append(attr, attraction)
-	encode(attraction)
+
 	return attr
 
 }
 
-func encode(s1 Attraction) {
+func encode(s1 Attraction) []byte {
 
 	bytes_s1, err := json.Marshal(s1)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(bytes_s1))
+	return bytes_s1
 }
 
-func decode(s1 []byte) {
+func decode(s1 []byte) Attraction {
 
 	var stu = &Attraction{}
 	var err = json.Unmarshal(s1, stu)
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("Unmarshal: Name: %s, InPark: %s ,Place : %s  ,  Manufacturer : %s \n", stu.Name, stu.InPark, stu.Place, stu.Manufacturer)
+	return *stu
 }
 
 func router() {
 
 	var size = 0
 	attractions := make([]Attraction, size)
+	http.Handle("/", http.FileServer(http.Dir("../static")))
 
 	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
-		get(attractions)
+		w.Header().Add("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		json.NewEncoder(w).Encode(attractions)
+
 	})
 	http.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request) {
 		var id = r.FormValue("Id")
@@ -140,6 +160,11 @@ func router() {
 
 		attractions = create(attractions, name, inPark, place, manufacturer)
 		fmt.Println(attractions)
+
+	})
+
+	http.HandleFunc("/saveInfile", func(w http.ResponseWriter, r *http.Request) {
+		saveInfile(attractions)
 
 	})
 
