5d3a808abcb00a6bff33b33ea326c05e2dc55abf
LeoEsgi <dleo@myges.fr>
2022-02-10 03:15:32 -0800

    rendu

--- src/tpSystem.go
+++ src/tpSystem.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 )
 
+// Nous avons utilisé une structure pour permettre de stocker nos processus
+// Cette structure nous permet d'optimiser le code
 type Process struct {
 	Pid string
 	Cwd string
@@ -17,6 +19,7 @@ func main() {
 	fmt.Println(pid())
 }
 
+//
 func pid() string {
 	var process []Process
 	f, err := os.Open("/proc")
@@ -47,6 +50,8 @@ func pid() string {
 
 }
 
+// inspecte le dossier lié au processus pour chercher si le dossier cwd
+// existe et si oui retourne son path
 func inspectPidCwd(id string) string {
 
 	file, err := os.Readlink("/proc/" + id + "/cwd")
@@ -59,6 +64,9 @@ func inspectPidCwd(id string) string {
 	}
 
 }
+
+// inspecte le dossier lié au processus pour chercher si le fichier exe
+// existe et si oui retourne son path
 func inspectPidExe(id string) string {
 
 	file, err := os.Readlink("/proc/" + id + "/exe")
@@ -70,6 +78,7 @@ func inspectPidExe(id string) string {
 	}
 }
 
+// fonction d'affichage  avec pour chaque processus , son pid , le chemin s'il existe de cwd et exe
 func display(process []Process) string {
 	var show string
 	for i := 0; i < len(process); i++ {
